0e1e59509af67f394bd459c79229f6c174d90e39
Artem_M <web.ep4@gmail.com>
2025-04-18 18:27:52 +0300

    Улучшения сайта

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 window.addEventListener('load', () => {
     document.body.style.opacity = 1;
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.collapsible-code').forEach(pre => {
-        const toggle = document.createElement('button');
-        toggle.className = 'code-toggle';
-        toggle.textContent = 'Свернуть';
-
-        // Вставляем кнопку ПЕРЕД блоком <code>
-        const codeBlock = pre.querySelector('code');
-        pre.insertBefore(toggle, codeBlock);
-
-        toggle.addEventListener('click', () => {
-            pre.classList.toggle('collapsed');
-            toggle.classList.toggle('collapsed');
-        });
-    });
-});
