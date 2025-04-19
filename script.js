623c7a3542526a288e99d36696e966927db73686
Artem_M <web.ep4@gmail.com>
2025-04-19 11:55:19 +0300

    Кнопка наверх, правки

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 window.addEventListener('load', () => {
     document.body.style.opacity = 1;
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollBtn = document.querySelector('.scroll-top');
+
+    // Показать/скрыть кнопку
+    window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollY > 400) {
+        scrollBtn.classList.add('visible');
+    } else {
+        scrollBtn.classList.remove('visible');
+    }
+    });
+
+    // Плавный скролл
+    scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    });
+});
